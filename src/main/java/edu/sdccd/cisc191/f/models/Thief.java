40dfb0ffb739beca9ba8961245d645bb0cd9ebe9
Noah <nwidderz@gmail.com>
2021-03-10 18:49:03 -0800

    test

---
+++ src/main/java/edu/sdccd/cisc191/f/models/Thief.java
@@ -0,0 +1,7 @@
+package edu.sdccd.cisc191.f.models;
+
+public class Thief extends Player {
+    public Thief (int health, int energy, int block, int gold) {
+        super(health, energy, block, gold);
+    }
+}
