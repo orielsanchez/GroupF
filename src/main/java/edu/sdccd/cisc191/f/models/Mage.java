c33e2f1fe66a1dc74aeb51f80956fee04b273097
Noah <nwidderz@gmail.com>
2021-03-10 18:41:45 -0800

    test

--- src/main/java/edu/sdccd/cisc191/f/models/Mage.java
+++ src/main/java/edu/sdccd/cisc191/f/models/Mage.java
@@ -1,5 +1,6 @@
 package edu.sdccd.cisc191.f.models;
 //comment here
+//another comment
 public class Mage extends Player {
     public Mage(int health, int energy, int block, int gold) {
         super(health, energy, block, gold);
