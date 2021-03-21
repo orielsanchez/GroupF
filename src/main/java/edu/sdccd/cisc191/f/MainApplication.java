1438a39e76e8f72c4e2b0fb09cd9b4a3d6bd6bd3
Oriel Sanchez <deathofacolour@icloud.com>
2021-03-21 00:23:07 -0700

    Updated Project to use JDK 8 Added starting window

--- src/main/java/edu/sdccd/cisc191/f/MainApplication.java
+++ src/main/java/edu/sdccd/cisc191/f/MainApplication.java
@@ -1,16 +1,36 @@
 package edu.sdccd.cisc191.f;
 
-public class MainApplication {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
     public static void main(String[] args) {
+
+        // Just here to test
         Player player = new Warrior(100, 5, 10, 10);
         Enemy ogre = new OgreEnemy(50, 10);
 
         Map map = new Map();
         map.generateListOfRooms();
 
-
-
         System.out.println(player.getHealth());
         System.out.println(ogre.isDead());
+
+        // JavaFX Launcher
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GridPane gridPane = new GridPane();
+        Scene scene = new Scene(gridPane, 600, 400);
+        primaryStage.setTitle("Slay The Spire Clone");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
     }
 }
